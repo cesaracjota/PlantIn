22d140709ed41e195fb6a18b3add95ab0e9b9b94
Cesar Acjota Merma <cesar.acjota@tecsup.edu.pe>
2022-04-19 12:36:08 -0500

    agregando pantalla menu principal

--- app/src/main/java/com/example/plantin/Login.kt
+++ app/src/main/java/com/example/plantin/Login.kt
@@ -1,11 +1,20 @@
 package com.example.plantin
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_login.*
+import kotlinx.android.synthetic.main.activity_main.*
 
 class Login : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
+
+        LogginButton.setOnClickListener(){
+            val x = Intent(this,MenuPrincipal::class.java)
+            startActivity(x)
+            finish()
+        }
     }
 }
